f8409781eea0ca719920ff830ae147a434c89805
MikeESL <mike.reser.03@gmail.com>
2014-05-23 11:29:57 -0400

    fixing controller Baub

--- public/js/controllers/shop.js
+++ public/js/controllers/shop.js
@@ -1,14 +1,14 @@
-var shopApp = angular.module('crudCartApp')
-	.controller ('BaubCtrl', ['$scope', '$routeParams', '$location', 'ShopService', function($scope, $routeParams, $location, ShopService){
+// var shopApp = angular.module('crudCartApp')
+// 	.controller ('BaubCtrl', ['$scope', '$routeParams', '$location', 'ShopService', function($scope, $routeParams, $location, ShopService){
 
-			$scope.things = ShopService.getAllThings();
-			$scope.addPost = function(){
-				$location.path('/newItem');
-			};
+// 			$scope.things = ShopService.getAllThings();
+// 			$scope.addPost = function(){
+// 				$location.path('/newItem');
+// 			};
 
-			// @params {thing} is an data object from cart.html object
-			$scope.createThing = function(thing) {
-				ShopService.createThing(thing);
-				$location.path('/cart');
-			};
-	}]);
+// 			// @params {thing} is an data object from cart.html object
+// 			$scope.createThing = function(thing) {
+// 				ShopService.createThing(thing);
+// 				$location.path('/cart');
+// 			};
+// 	}]);
